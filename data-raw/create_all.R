54566b81f9dd8250c5d1dce93c05838547a8c482
fuerzhou <653540853@qq.com>
2022-01-05 11:54:41 +1100

    add a new shape

--- data-raw/create_all.R
+++ data-raw/create_all.R
@@ -1,3 +1,3 @@
 # run `use_this` all together for all internal items
 
-usethis::use_data(C_shape1, C_shape2, bg1, overwrite = T, internal = T)
+usethis::use_data(C_shape1, C_shape2, C_shape3, bg1, overwrite = T, internal = T)
