a8e8ed6335bfe9483bcbccbcbfba790dddab13ba
fuerzhou <653540853@qq.com>
2022-02-11 00:07:20 +1100

    simplify the params

--- R/simulate_mixing.R
+++ R/simulate_mixing.R
@@ -2,22 +2,14 @@
 #'
 #' @param background_sample Data.Frame The image that the stripes are simulated on
 #' @param names_of_mixing Vector of the mixed cell types
-#' @param shape String of the shape of the mixing area. Only "Rectangle" is available now
-#' @param size Vector c(width, height) The size of the mixing area
 #' @param mixing_degree Vector of the proportions of the mixed cell types
-#' @param centre_loc Data.frame The 2D location of the centre of the mixed area
-#' @param win owin object output from spatstat.geom::owin function
 #'
 #' @return A data.frame of the simulated image
 #' @export
 #'
 simulate_mixing <- function(background_sample,
                             names_of_mixing = c("Tumour", "Immune", "Others"),
-                            shape = "Rectangle",
-                            size = c(2500,2000),
-                            mixing_degree = c(0.2, 0.4, 0.4),
-                            centre_loc = data.frame("x" = c(1250), "y" = c(1000)),
-                            win = spatstat.geom::owin(c(0,2500),c(0,2000))) {
+                            mixing_degree = c(0.2, 0.4, 0.4)) {
 
   # CHECK is the background sample a dataframe?
   if (!is.data.frame(background_sample)) {
@@ -28,41 +20,27 @@ simulate_mixing <- function(background_sample,
     background_sample[, "Phenotype"] <- "Others"
   }
 
-  # generate a location as the centre of the cluster
-  if (is.null(centre_loc)){
-    seed_point <- spatstat::runifpoint(1, win=win)}
-  else seed_point <- centre_loc
-  a <- seed_point$x
-  b <- seed_point$y
-
   n_types <- length(names_of_mixing)
-
-  # size of the rectangle
-  w <- 0.5*size[1]
-  h <- 0.5*size[2]
-
   for (i in 1:dim(background_sample)[1]){
     x <- background_sample[i, "Cell.X.Position"]
     y <- background_sample[i, "Cell.Y.Position"]
 
-    if (x > a - w && x < a + w && y > b - h && y < b + h){
-      random <- stats::runif(1)
+    random <- stats::runif(1)
 
-      # if the random number falls in the range of an infiltration proportion,
-      # pheno will be the corresponding infiltraiton type
-      n <- 1 # start from the first proportion
-      current_p <- 0
-      while (n <= n_types){
-        current_p <- current_p + mixing_degree[n]
-        if (random <= current_p) {
-          pheno <- names_of_mixing[n]
-          break
-        }
-        n <- n+1
+    # if the random number falls in the range of an infiltration proportion,
+    # pheno will be the corresponding infiltraiton type
+    n <- 1 # start from the first proportion
+    current_p <- 0
+    while (n <= n_types){
+      current_p <- current_p + mixing_degree[n]
+      if (random <= current_p) {
+        pheno <- names_of_mixing[n]
+        break
       }
-
-      background_sample[i, "Phenotype"] <- pheno
+      n <- n+1
     }
+    background_sample[i, "Phenotype"] <- pheno
   }
+
   return(background_sample)
 }
