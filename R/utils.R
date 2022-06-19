d240c7cae5c86a9ba9fa1b0d758959c1290e16de
fuerzhou <653540853@qq.com>
2022-06-20 09:51:57 +1000

    fix bug

--- R/utils.R
+++ R/utils.R
@@ -3,7 +3,6 @@ get_colData <- function(spe_object){
     formatted_data <- data.frame(SummarizedExperiment::colData(spe_object))
     formatted_data <- cbind(formatted_data,
                             data.frame(spatialCoords(spe_object)))
-    formatted_data <- formatted_data %>% tibble::rownames_to_column("Cell.ID")
 
     # delete column `sample_id`
     formatted_data$sample_id <- NULL
